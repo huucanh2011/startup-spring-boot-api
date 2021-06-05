d2647b881f6df2a5a1f54ab81eb87385c324ad91
CanhNH <huucanh20111998@gmail.com>
2021-06-05 17:21:09 +0700

    refactor reset password request dto

--- src/main/java/vn/luvina/startup/dto/auth/ForgotPasswordRequestDto.java
+++ src/main/java/vn/luvina/startup/dto/auth/ForgotPasswordRequestDto.java
@@ -1,10 +1,19 @@
 package vn.luvina.startup.dto.auth;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class ForgotPasswordRequestDto {
 
+  @ApiModelProperty(value = "email", example = "example@example.xyz")
+  @NotBlank(message = "Email không được trống.")
+  @Email(message = "Email không đúng định dạng.")
+  @Size(message = "Email tối đa 60 kí tự.", max = 60)
   private String email;
 
 }
--- src/main/java/vn/luvina/startup/dto/auth/ResetPasswordRequestDto.java
+++ src/main/java/vn/luvina/startup/dto/auth/ResetPasswordRequestDto.java
@@ -1,14 +1,28 @@
 package vn.luvina.startup.dto.auth;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class ResetPasswordRequestDto {
 
+  @ApiModelProperty(value = "email", example = "example@example.xyz")
+  @NotBlank(message = "Email không được trống.")
+  @Email(message = "Email không đúng định dạng.")
+  @Size(message = "Email tối đa 60 kí tự.", max = 60)
   private String email;
 
+  @ApiModelProperty(value = "password", example = "password")
+  @NotBlank(message = "Mật khẩu không được trống.")
+  @Size(message = "Mật khẩu từ 6 đến 60 kí tự.", min = 6, max = 60)
   private String password;
 
+  @ApiModelProperty(value = "resetToken", example = "token")
+  @NotBlank(message = "Reset token không được trống.")
   private String resetToken;
 
 }
